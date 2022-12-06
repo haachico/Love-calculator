203f72edc1261bcff113bac43fcb34933f117537
Nilesh <nilesh@haachico.com>
2022-12-06 15:41:28 +0530

    add styling

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ function loveOutput() {
   const name1 = nameEl1.value;
   const name2 = nameEl2.value;
   if (name1.length < 3) {
-    statementBox.innerText = `Please enter atleast 3 letters`;
+    statementBox.innerText = `Please enter atleast 3 letters.`;
   }
   if (name2.length < 3) {
     statementBox.innerText = `Please enter atleast 3 letters`;
   } else {
     const percentage = lovePercentage(name1, name2);
     percentBox.innerText = `${name1} and ${name2}'s love percentage is ${percentage}% :)`;
-    if (percentage <= 50) {
-      statementBox.innerText = `The relationship is okayish`;
+    if (percentage >= 0 && percentage < 20) {
+      statementBox.innerText = `The relationship is very bad. 🥲`;
+    } else if (percentage >= 20 && percentage <= 50) {
+      statementBox.innerText = `The relationship is okayish. 😶 `;
+    } else if (percentage >= 51 && percentage <= 70) {
+      statementBox.innerText = `The relationship is good!😊`;
     } else {
-      statementBox.innerText = `The result is grreat`;
+      statementBox.innerText = `The relationship is great!! 😍😘`;
     }
   }
 }
